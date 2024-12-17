5a5e3938d04c8a3553522f0e41c1f634f13a11d1
Prince-Rathore7869 <princerathore4321@gmail.com>
2024-12-17 16:43:51 +0530

    Bank Account class has been created to initalize the customer means creating the new customer , also in that class deposit and withdraw function for operation and some bugs fixed

---
+++ bank_account.rb
@@ -0,0 +1,35 @@
+module Account
+ class Bank_Account
+  attr_reader :account_number,:name, :balance
+  @@account_number=100
+  
+  #To generate uniqie account number
+  def initialize(name,initial_deposit)
+    @name=name
+    @balance=initial_deposit
+    @account_number=@@account_number+=1
+  end
+
+  #Deposit money
+  def deposit(amount)
+    @balance +=amount
+    puts "Deposit successful!
+    New balance: $#{balance}"
+  end
+
+  #Withdraw money
+  def withdraw(amount)
+    if amount> @balance
+      puts "Insufficient fund! Current balance:$#{balance}"
+    else 
+      @balance -=amount
+      puts "Withdrawal Successful! Remaining balance:$#{balance}"
+    end
+  end
+
+  #check_balance
+  def check_balance
+    puts "Account number: #{@account_number} | Name: #{name} | Balance: $#{balance}"
+  end
+ end
+end
