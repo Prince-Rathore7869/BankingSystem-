6953d0c5592e912f3eb170e18503eaaa8ca5650b
Prince-Rathore7869 <princerathore4321@gmail.com>
2024-12-18 14:15:50 +0530

    The customer attribute was added and also fix the problem with storing the data with array now the object will generate from the hash

--- bank_account.rb
+++ bank_account.rb
@@ -1,13 +1,35 @@
 module Account
  class BankAccount
-  attr_reader :account_number,:name, :balance
+  attr_reader :account_number,:name, :balance, :customer_id, :aadhar_number
   @@account_number=100
+  @@customer_id=0
   
   #To generate uniqie account number
-  def initialize(name,initial_deposit)
+  def initialize(name,initial_deposit,aadhar_number)
     @name=name
     @balance=initial_deposit
+    @aadhar_number=aadhar_number
     @account_number=@@account_number+=1
+    @customer_id=@@customer_id+=1
+  end
+ 
+  #this function will convert the object to hash
+  def to_hash
+    {
+      name: @name,
+      balance: @balance,
+      aadhar_number: @aadhar_number,
+      customer_id: @customer_id,
+      account_number: @account_number
+    }
+  end
+
+  #This is will create the new object from hash
+  def self.new_from_hash(hash)
+    account=new(hash[:name],hash[:balance],hash[:aadhar_number])
+    account.account_number=hash[:account_number]
+    account.customer_id=hash[:customer_id]
+    account
   end
 
   #Deposit money
