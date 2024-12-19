1ce2d4b56b41bb61a9807f0508e2c1d7c4a46ab1
Prince-Rathore7869 <princerathore4321@gmail.com>
2024-12-19 19:26:42 +0530

    Validate module was created to handle the edge cases, this will check the customer id and amount so the OOPs concept was applied here for code reusuability

---
+++ validate_inputs.rb
@@ -0,0 +1,19 @@
+module Validate
+  def get_validated_id(prompt)
+    loop do
+      print prompt
+      input=gets.chomp
+      return input.to_i if input.match?(/^\d+$/)
+      puts "Invalid Input.Customer ID must be Integer."
+    end
+  end
+
+  def get_validated_amount(prompt)
+    loop do
+      print prompt
+      input=gets.chomp
+      return input.to_f if input.match?(/^\d+(\.\d{1,2})?$/)     #from Google
+      puts "Invalid input.Amount must be Positive number."
+    end
+  end
+end
