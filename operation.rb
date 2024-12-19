8e3048dd4d49602519942f51a4f5af8b1e201834
Prince-Rathore7869 <princerathore4321@gmail.com>
2024-12-19 13:22:03 +0530

    Successfully implmented the hash storage instaed of array because it was creating the problem when searching the customer taking the time complexity of O(n)

--- operation.rb
+++ operation.rb
@@ -1,17 +1,18 @@
 require_relative 'employee_file'
 require_relative 'bank_account'
 require_relative 'dummy_data'
+require_relative 'transaction'
 
 module Bankoperation
   include Employee
   include Account
   include TestModule
+  include Transaction
   
   class BankingSystem
     def initialize
-      @accounts=[]
-      dummy_data(@accounts)
-      
+      @accounts= {}              #it holds the inistance of Banking System
+      dummy_data(@accounts)      #populate the dummy data
     end
     def run
       employee=EmployeeLogin.new
@@ -26,7 +27,8 @@ module Bankoperation
         puts "4. Check balance"
         puts "5. Search a Customer"
         puts "6. List All Customer"
-        puts "7. Exit"
+        puts "7. Transfer Amount"
+        puts "8. Exit"
 
         print "Choose an option: "
         choice=gets.chomp.to_i
@@ -45,6 +47,8 @@ module Bankoperation
         when 6
            list_all_customers
         when 7
+            transfer_amount
+        when 8
           puts "Exiting the banking System.Goodbye"
           break 
         else
@@ -57,7 +61,7 @@ module Bankoperation
     def register_customer
       print "Enter Customer Name: "
       name=gets.chomp
-      print "Enter your Aadhar Number"
+      print "Enter your Aadhar Number:"
       aadhar_number=gets.chomp
       if aadhar_number.match?(/^\d{12}$/)
       
@@ -68,7 +72,7 @@ module Bankoperation
         puts "Initial deposit cannot be negative."
        else
         account=BankAccount.new(name,initial_deposit,aadhar_number)
-        @accounts<<account
+        @accounts[account.customer_id]=account
         puts "Customer registered successfully!"
         puts "Customer Id:#{account.customer_id}, Account Number:#{account.account_number}"
        end 
@@ -81,15 +85,10 @@ module Bankoperation
     def list_all_customers
       if @accounts.empty?
         puts "No customers registered yet. Please add some customers."
-      else
+      end
         puts "-------List of All Customers--------"
-        @accounts.each do |account|
-          puts "Name:#{account.name}"
-          puts "Customer ID:#{account.customer_id}"
-          puts "Account Number:#{account.account_number}"
-          puts "Aadhar No.:#{account.aadhar_number}"
-          puts "balance:#{account.balance}"
-        end
+        @accounts.each_value do |account|
+          puts "Customer ID:#{account.customer_id}, Name:#{account.name},Balance:#{account.balance},AadharNo:#{account.aadhar_number}"
       end
     end
     
@@ -97,8 +96,7 @@ module Bankoperation
     def serach_customer_by_id
       print "Enter the customer id you want to search:"
       customer_id=gets.chomp.to_i
-       
-      customer=@accounts.find {|acc| acc.customer_id== customer_id}
+      customer=@accounts[customer_id]
       if customer
         puts "--------Customer Details--------"
         puts "Name:#{customer.name}"
@@ -111,54 +109,42 @@ module Bankoperation
       end
     end
 
-    #Find an account by account number
-    def find_account
-      print "Enter Account Number: "
-      account_number=gets.chomp.to_i
-      @accounts.find {|acc| acc.account_number==account_number }
-    end
-
     #Perform deposit
     def perform_deposit
-      account=find_account
-      if account
+      print "Enter Customer ID for Deposit: "
+      customer_id=gets.chomp.to_i
+      account=@accounts[customer_id]
+      if account.nil?
+        puts "Customer with ID #{customer_id} not found."
+        return
+      end
         print "Enter Deposit Amount: "
         amount=gets.chomp.to_f
-        if amount<=0
-          puts 'Deposit amount must be greater than 0.'
-        else
-          account.deposit(amount)
-          
-        end
-      else
-        puts "account not found!"
-      end
+        account.deposit(amount)
     end
 
     #Perform Withdrawal
     def perform_withdraw
-      account= find_account
-      if account
-        print "Enter the Withdrawal Amount: "
-        amount =gets.chomp.to_f
-        if amount<= 0
-          puts "Withdrawal amount must be greater than 0"
-        else
-          account.withdraw(amount)
-        end
-      else
-        puts "Account not found!"
+      print "Enter Customer ID for Withdrawal:"
+      customer_id=gets.chomp.to_i
+      account=@accounts[customer_id]
+      if account.nil?
+        puts "Customer ID #{customer_id} not found. "
       end
+      print "Enter withdrawal amount: " 
+      amount=gets.chomp.to_f
+      account.withdraw(amount)
     end
 
     #perform balance chceck
     def perform_check_balance
-      account= find_account
-      if account
-        account.check_balance
-      else
+      print "Enter the Customer ID to check balance: "
+      customer_id=gets.chomp.to_i
+      account=@account[customer_id]
+      if account.nil?
         puts "Account not found !"
       end
+      account.check_balance
     end
   end
 end
